676c4f7e134e9ebe411cd793ff8179e80e5a257b
Ruto <devruto@gmail.com>
2026-08-11 20:01:45 -0400

    Set dark theme

--- globalapi-replayviewer/nuxt.config.ts
+++ globalapi-replayviewer/nuxt.config.ts
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: { class: 'dark' }
+    }
+  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()]
